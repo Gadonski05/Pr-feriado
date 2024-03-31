55ea9b76e81a91a43e8fd45734554cc41e928bfc
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-31 15:55:57 -0300

    Class principal
    Puxa a Class principal do outro código e cria uma principal do main e vai declarando as variáveis com os respectivos valores que o usuário pode colocar.

--- MainPs.java
+++ MainPs.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class MainPs {
     
+    public static void main(String[] args) {
+        
+        Posto bomba = new Posto("Gasolina", 6.0, 150.0);
+
+        bomba.Abastece(100);
+        bomba.MostraInf();
+        bomba.AbasteceLit(15);
+        bomba.MostraInf();
+    }
 }
