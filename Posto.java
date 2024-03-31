92e21a078dfe64016524102b772c229938c3adf5
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-31 15:18:11 -0300

    Criando o mecanismo de abastecer

--- Posto.java
+++ Posto.java
@@ -3,4 +3,18 @@ public class Posto{
     private String Combustivel;
     private double ValL;
     private double QtdCom;
+
+    public Posto(String Combustivel, double ValL, double QtdCom){
+
+        this.Combustivel = Combustivel;
+        this.ValL = ValL;
+        this.QtdCom = QtdCom;
+
+    }
+
+    public void Abastece(double Val){
+        if (LitrosAb) {
+            
+        }
+    }
 }
